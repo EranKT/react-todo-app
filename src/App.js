3489eb7840c5c06bc66e22c11308f07a660cbd02
EranKT <eran@ktfgalaxy.com>
2020-12-26 12:24:06 +0100

    created new components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Todo-app">
       <h1>Todo app</h1>
     </div>
   );
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import React, { useState } from "react";
+
+function TodoForm() {
+  return <div></div>;
+}
+
+export default TodoForm;
