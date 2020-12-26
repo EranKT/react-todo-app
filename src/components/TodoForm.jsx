24383c4a9ce4f3c2623b6a1e9da028456cd9fe09
EranKT <eran@ktfgalaxy.com>
2020-12-26 12:49:01 +0100

    implemented the add todo form

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,8 +1,33 @@
-import { useState } from "react";
 import React, { useState } from "react";
 
-function TodoForm() {
-  return <div></div>;
+function TodoForm(props) {
+  const [input, setInput] = useState("");
+
+  const handeChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
+  };
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="add a todo"
+        value={input}
+        name="text"
+        className="todo-input"
+        onChange={handeChange}
+      />
+      <button className="todo-button">Add todo</button>
+    </form>
+  );
 }
 
 export default TodoForm;
